f4284b5a14824985b019c538d186fb09d7bbb886
Megan <893669153@qq.com>
2017-09-29 09:10:18 +0800

    test

--- demo/src/components/Item.js
+++ demo/src/components/Item.js
@@ -3,31 +3,53 @@
  * @Author: chen_huang 
  * @Date: 2017-09-27 23:49:24 
  * @Last Modified by: chen_huang
- * @Last Modified time: 2017-09-28 01:02:17
+ * @Last Modified time: 2017-09-29 01:14:07
  */
 import React, { Component } from 'react'
 import {
     StyleSheet,
     View,
     Text,
-    Image
+    Image,
+    Dimensions
 } from 'react-native'
 
+const { width, height } = Dimensions.get('window')
+
+const thirdWidth = width / 3
+
+const imageWidth = thirdWidth - 10 * 2
+
+const imageHeight = thirdWidth / 0.697
+
 const styles = StyleSheet.create({
+    root: {
+        width: imageWidth,
+        marginRight: 15
+    },
     image: {
-        flex: 1,
-        padding: 15,
-        backgroundColor: 'red'
+        width: imageWidth,
+        height: imageHeight
+    },
+    title: {
+        textAlign: 'center',
+        marginBottom: 10
     }
 })
 
 // 无状态组件
 const Item = (props) => {
-    const { title } = props
+    const { title, image } = props
     return (
-        <View>
-            
-            <Text style = {styles.image}>{ title }</Text>
+        <View style = { styles.root }>
+            <Image 
+                source = {{ uri: image }}
+                style = { styles.image }
+            ></Image>
+            <Text 
+                numberOfLines = { 1 }
+                style = { styles.title }
+            >{ title }</Text>
         </View>
     )
 }
--- demo/src/myApp.js
+++ demo/src/myApp.js
@@ -3,7 +3,7 @@
  * @Author: chen_huang 
  * @Date: 2017-09-27 23:42:42 
  * @Last Modified by: chen_huang
- * @Last Modified time: 2017-09-28 01:12:10
+ * @Last Modified time: 2017-09-29 01:30:53
  */
 import React, { Component } from 'react'
 import {
@@ -16,49 +16,85 @@ import {
 
 import Item from './components/Item'
 
-const json = [
+const movies = [
     {
         id: '0',
-        title: '张三'
+        title: '张三',
+        images:  {
+            medium: 'http://facebook.github.io/react/img/logo_og.png'
+        }
     },
     {
         id: '1',
-        title: '李四'
+        title: '李四',
+        images:  {
+            medium: 'http://facebook.github.io/react/img/logo_og.png'
+        } 
     },
     {
         id: '2',
-        title: '王五'
+        title: '王五',
+        images:  {
+            medium: 'http://facebook.github.io/react/img/logo_og.png'
+        }
     },
     {
         id: '3',
-        title: '老刘'
+        title: '老刘',
+        images:  {
+            medium: 'http://facebook.github.io/react/img/logo_og.png'
+        }
     }
 ]
 
 export default class MyApp extends Component {
+    state = {
+        movies: movies,
+        // 下拉刷新开关
+        refreshing: false
+    }
+    refreshing  = false
     fetchDate = () => {
+        // 为了防止多次连续下拉
+        if (this.refreshing) return
+        this.setState({
+            refreshing: true
+        })
+        this.refreshing = true
         fetch('http://api.douban.com/v2/movie/in_theaters')
             .then((response) => response.text())
             .then((res) => {
-                const dateJson = JSON.parse(res)
-                return dateJson
+                const dataJson = JSON.parse(res)
+                console.log(dataJson)
+                this.setState({
+                    movies: dataJson.subjects,
+                    refreshing: false
+                })
+                this.refreshing = false
             })
             .catch((error) => {
                 console.log(error)
             })
     }
     componentDidMount() {
-        this.fetchDate()
+        // this.fetchDate()
     }
     render () {
+        const { movies, refreshing } = this.state
         return (
             <View>
                 <FlatList 
                     keyExtractor = {item => item.id}
-                    numColumns = {3}
+                    numColumns = { 3 }
                     columnWrapperStyle = {styles.row}
-                    data = { json }
-                    renderItem = {({item}) => <Item title = {item.title}/>}
+                    data = { movies }
+                    onRefresh = { this.fetchDate }
+                    refreshing = { refreshing }
+                    renderItem = {({ item }) => 
+                        <Item 
+                            title = { item.title }
+                            image = { item.images.medium }
+                        />}
                 />
             </View>
         )
@@ -67,7 +103,6 @@ export default class MyApp extends Component {
 
 const styles = StyleSheet.create({
     row: {
-        justifyContent: 'space-between',
         paddingHorizontal: 15,
     }
 })
